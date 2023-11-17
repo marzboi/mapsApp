baf6dcb0e279bf693164d4b1cdca54b9a9727ac7
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-11-17 13:24:58 +0100

    Add minimap

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrls: ['./mini-map.component.css']
+  styleUrls: ['./mini-map.component.css'],
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
 
+  ngAfterViewInit() {
+    if (!this.divMap?.nativeElement) throw 'Map Div not found';
+    if (!this.lngLat) throw "LngLat can't be null";
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
 }
